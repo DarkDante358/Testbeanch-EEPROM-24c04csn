750df5c57daa4b84997cda3408eacc0c3b5dc766
jakwoj9933 <245731@student.pwr.edu.pl>
2020-05-17 20:20:38 +0200

    Dodane komponenty do test bencha

---
+++ testbeanch_EEPROM/testbeanch_EEPROM.srcs/sources_1/new/EEPROM.vhd
@@ -0,0 +1,17 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity EEPROM is
+    Port ( RESET : in STD_LOGIC;
+           WP : in STD_LOGIC;
+           A : in STD_LOGIC_VECTOR (2 downto 0);
+           SCL : in STD_LOGIC;
+           SDA : inout STD_LOGIC);
+end EEPROM;
+
+architecture Behavioral of EEPROM is
+
+begin
+
+
+end Behavioral;
