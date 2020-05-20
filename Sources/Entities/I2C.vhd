7f0f981da05038dbc755be241d115283317dcfc5
jakwoj9933 <245731@student.pwr.edu.pl>
2020-05-20 20:04:38 +0200

    I2C dev

--- Sources/Entities/I2C.vhd
+++ Sources/Entities/I2C.vhd
@@ -24,19 +24,19 @@ end I2C;
 architecture Behavioral of I2C is
     constant WORD_SIZE : integer := 8;
 
-    type  i2c_states IS (wait_for_start, start, recive_data, stop);
+    type  i2c_states IS (wait_for_start, start, recive_data, stop, send_ack, send_data);
 	signal state, next_state : i2c_states := wait_for_start;
     
-    signal SCL_counter : STD_LOGIC_VECTOR (2 downto 0) := "000";
+    signal SCL_counter : integer := 0;
     
     signal internal_SDA : STD_LOGIC := 'Z';
 begin
 
-    reg : process(SCL, RESET) -- process which switches states
+    reg : process(next_state, RESET) -- process which switches states
     begin
         if (RESET='1') then
             state <= wait_for_start;
-        elsif(rising_edge(SCL)) then
+        else
             state <= next_state;
         end if;
     end process reg;
@@ -58,7 +58,7 @@ begin
                     end if;
                     
                 when recive_data => 
-                    if(SCL_counter = std_logic_vector(to_unsigned((WORD_SIZE + 1), 3))) then
+                    if(SCL_counter = WORD_SIZE + 1) then
                         next_state <= stop;
                     end if;
                     
@@ -69,6 +69,18 @@ begin
             end case;
         end if;
     end process machine;
+    
+    scl_counter_proc : process (SCL, RESET, STATE)
+    begin
+
+        if (state = stop and RESET = '1') then
+            scl_counter <= 0;
+        elsif(state = recive_data and rising_edge(SCL) and scl_counter <= WORD_SIZE + 1) then
+            scl_counter <= scl_counter + 1;
+        end if;
+            
+    end process scl_counter_proc;
+    
 
     SDA <= internal_SDA;
 
