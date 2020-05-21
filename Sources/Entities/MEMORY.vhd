c6731a380e2c0bb52ee394fd6d33aa4b855b7ce5
jakwoj9933 <245731@student.pwr.edu.pl>
2020-05-21 21:27:03 +0200

    Memory and testbench done

--- Sources/Entities/MEMORY.vhd
+++ Sources/Entities/MEMORY.vhd
@@ -21,21 +21,24 @@ architecture Behavioral of MEMORY is
     type cell_structure is array (0 to 511) of std_logic_vector(7 downto 0);
     signal memory : cell_structure := (others => (others => '0'));
     
+    signal internal_WORD : STD_LOGIC_VECTOR(7 downto 0) := (others => 'Z'); -- signal temporarily carrying loaded word
+    
 begin
 
    mem : process (RESET, RW)
    begin
-    
     if(RESET = '1') then
         memory <= (others => (others => '0')); -- memory cleared
     elsif(RW /= 'Z') then -- Waiting for CellAdd and WORD
-        if(RW = '1') then -- Read mode
-            WORD <= memory(to_integer(unsigned(CelAdd)));
+        if(RW = '0') then -- Read mode
+            internal_WORD <= memory(to_integer(unsigned(CelAdd)));
         else -- Write mode
             memory(to_integer(unsigned(CelAdd))) <= WORD;
         end if;
     end if;
  
    end process mem;
+   
+   WORD <= internal_WORD when RW='0' else (others => 'Z');
 
 end Behavioral;
--- Sources/Testbench/MEMORY_Testbench.vhd
+++ Sources/Testbench/MEMORY_Testbench.vhd
@@ -14,14 +14,16 @@ end MEMORY_Testbench;
 architecture behave of MEMORY_Testbench is
     
     signal t_CelAdd :  STD_LOGIC_VECTOR (7 downto 0);
-    signal t_WORD:  STD_LOGIC_VECTOR (7 downto 0);
+    signal t_WORD:  STD_LOGIC_VECTOR (7 downto 0) := (others => 'Z');
     signal t_RW:  STD_LOGIC := 'Z';
     signal t_RESET:  STD_LOGIC ;
+    --signal t_GET_RESPONSE: STD_LOGIC := '0';
     
     component MEMORY is
         Port ( CelAdd : IN STD_LOGIC_VECTOR (7 downto 0);
            WORD: INOUT STD_LOGIC_VECTOR (7 downto 0);
            RW: IN STD_LOGIC;
+           --GET_RESPONSE : IN STD_LOGIC;
            RESET: IN STD_LOGIC);
     end component MEMORY;
  
@@ -31,40 +33,48 @@ architecture behave of MEMORY_Testbench is
      Port map ( CelAdd => t_CelAdd,
            WORD => t_WORD,
            RW => t_RW,
+           --GET_RESPONSE => t_GET_RESPONSE,
            RESET => t_RESET); 
       
 
   sim : process
   begin
+  -- Reset 
   t_RESET <= '1';
   wait for 10ns;
   t_RESET <= '0';
   wait for 10ns;
-  t_celAdd <= "01000000";
-  t_WORD <= X"4c"; 
-  wait for 10ns;
-  t_RW <= '0';
-  wait for 10ns;
-  t_RW <= 'Z';
-  wait for 10ns;
-  t_celAdd <= "00100000";
-  t_WORD <= X"4e"; 
-  wait for 10ns;
-  t_RW <= '0';
-  wait for 10ns;
-  t_RW <= 'Z';
-  wait for 10ns;
-  t_celAdd <= "00100000";
-  wait for 10ns;
-  t_RW <= '1';
-  wait for 10ns;
-  t_RW <= 'Z';
-  wait for 10ns;
-  t_celAdd <= "01000000";
-  wait for 10ns;
-  t_RW <= '1';
-  wait for 100ns;
   
+  t_celAdd <= "00000000"; -- selecting word cell 0
+  t_WORD <= "00001010"; -- DEC 10 to data lines
+  wait for 10ns;
+  t_RW <= '1'; -- write mode
+  wait for 10ns;
+  t_RW <= 'Z'; -- disconnect
+  wait for 10ns;
+  
+  t_celAdd <= "00000001"; -- selecting word cell 1
+  t_WORD <= "00010100"; -- DEC 20 to data lines
+  wait for 10ns;
+  t_RW <= '1'; -- write mode
+  wait for 10ns;
+  t_RW <= 'Z'; -- disconnect
+  wait for 10ns;
+  
+  t_WORD <= (others => 'Z'); -- t_WORD to "IN" mode
+  
+  wait for 10ns;
+  t_celAdd <= "00000000"; -- selecting word cell 0
+  t_RW <= '0';  -- read mode
+  wait for 10ns;
+  t_RW <= 'Z'; -- disconnect
+  wait for 10ns;
+  
+  t_celAdd <= "00000001"; -- selecting word cell 1
+  t_RW <= '0'; -- read mode
+  wait for 10ns;
+  t_RW <= 'Z'; -- disconnect
+  wait for 10ns;
   
   assert false report "Tests Complete" severity failure;
   end process sim;
